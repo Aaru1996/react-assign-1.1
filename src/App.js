4b7aacea59a4c61cb4d458b5185e6a13f750dde2
Aaru1996 <arunkumaryadav19031996@gmail.com>
2022-05-25 23:04:04 +0530

    assign added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h3>Mobile Operating System</h3>
+    
+       <ul>
+         <li>Android</li>
+         
+         <li>Blackberry</li>
+         <li>iPhone</li>
+         <li>Windows Phone</li>
+       </ul>
+     <h3>Mobile Manufacturers</h3>  
+     <ul>
+         <li>Samsung</li>
+         <li>HTC</li>
+         <li>Micromax</li>
+         <li>Apple</li>
+       </ul>
+    </div>
+  );
+}
+
+export default App;
